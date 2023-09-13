2d1e62a075946fa3519a5c6e642ac03b9dd457da
XC-NCTR <83054794+XC-NCTR@users.noreply.github.com>
2023-09-13 09:26:53 -0500

    Update run.sh

--- script/run.sh
+++ script/run.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-#
-#$ -N iDILI
-#$ -S /bin/bash
-#$ -cwd
-#$ -j y
-#$ -l h_vmem=20G
-#$ -pe smp 12
-#$ -R y
-#$ -q hpc.q@ncshpcgpu01.fda.gov
-#$ -w e
-#$ -o /account/xchen/workspace/AnimalGAN/github/run/train.log
-#$ -e /account/xchen/workspace/AnimalGAN/github/run/train.err
 
 source /account/xchen/set_conda_env.sh
 
