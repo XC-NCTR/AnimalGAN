75d1dd8f83d7d6c3a930be4fbc67fc63eb8e223c
XC-NCTR <83054794+XC-NCTR@users.noreply.github.com>
2023-09-20 14:28:26 -0500

    Update calMolDes.py

--- SRC/calMolDes.py
+++ SRC/calMolDes.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     result_3d = list(result_3d)
     data = [result_3d[i].fill_missing(0).asdict() for i in range(len(result_3d))]
     df = pd.DataFrame(data, index=[SDFs[i].strip('.sdf') for i in range(len(SDFs))])
-    df.to_csv(os.path.join(path, 'MolDescriptors_3D.tsv'), sep='\t')
+    df.to_csv(os.path.join(path, 'Data','MolecularDescriptors.tsv'), sep='\t')
